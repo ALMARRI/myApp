de646da14fac21cdaddbc46c205e45c30d15bf79
Mohammad Almarri <almarri.cs@gmail.com>
2017-09-13 16:35:30 +0300

    2nd

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		// this is a comment for all
+		// 2nd update
 		String dString = scanner.nextLine();
 		boolean f = UserMainCode.validateDate(dString);
 		if (f) {
