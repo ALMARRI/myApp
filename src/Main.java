34a0e64484b359b14ddadd88c6468c01241ba3d5
Mohammad Almarri <almarri.cs@gmail.com>
2017-09-13 16:23:43 +0300

    updated line with comment

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+		// this is a comment for all
 		String dString = scanner.nextLine();
 		boolean f = UserMainCode.validateDate(dString);
 		if (f) {
